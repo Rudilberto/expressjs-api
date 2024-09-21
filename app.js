e0c9959d08c3edbabaf6d518b61be10090749bc3
Rudi <rudi_neitzke@yahoo.com.br>
2024-09-20 21:11:36 -0300

    add get driver per id and standing

--- app.js
+++ app.js
@@ -11,6 +11,21 @@ app.get(baseAPIRoute + "/drivers", (request, resp) => {
   resp.status(200).send(drivers);
 });
 
+// o :position é um parametro de rota, que nada mais é que uma variavel com o parametro da url
+app.get(baseAPIRoute + "/drivers/standings/:position", (request, resp) => {
+  // a request tem o campo .params, que por sua vez pode usar o nome do parametro :position
+  const { position } = request.params;
+  const selectedDriver = drivers[position - 1];
+  resp.status(200).send(selectedDriver);
+});
+
+// igual o de cima mas com id
+app.get(baseAPIRoute + "/drivers/:id", (request, resp) => {
+  const { id } = request.params;
+  const selectedDriver = drivers.find((driver) => driver.id === id);
+  resp.status(200).send(selectedDriver);
+});
+
 // Criando a porta local
 const port = 3000;
 // Escutando a porta local
